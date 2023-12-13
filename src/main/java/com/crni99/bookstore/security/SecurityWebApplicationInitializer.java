3b4cb224b4573f85fae0c65a7ae2e29e7dcac497
HafoChan <nshao159@gmail.com>
2023-12-13 13:51:27 +0700

    book

---
+++ src/main/java/com/crni99/bookstore/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+/*
+ * package com.crni99.bookstore.security;
+ * 
+ * import org.springframework.security.web.context.
+ * AbstractSecurityWebApplicationInitializer;
+ * 
+ * public class SecurityWebApplicationInitializer extends
+ * AbstractSecurityWebApplicationInitializer {
+ * 
+ * }
+ */
---
+++ src/main/java/com/crni99/bookstore/service/EmailService.java
@@ -0,0 +1,26 @@
+package com.crni99.bookstore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	
+	@Autowired
+	private final JavaMailSender mailSender;
+
+	public EmailService(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	public void sendEmail(String to, String subject, String message) {
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(to);
+		mailMessage.setSubject(subject);
+		mailMessage.setText(message);
+		mailSender.send(mailMessage);
+	}
+
+}
